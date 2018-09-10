2629385e7ad92b62c028d8fb3e6644c5b33da65b
Unknown <fribolin.stecker@gmail.com>
2018-09-10 18:26:34 +0200

    Blink + Angry

--- Feeling.cpp
+++ Feeling.cpp
@@ -5,34 +5,34 @@ Feeling::Feeling() {
   right.begin(0x70);
 }
 
-void Feeling::setState(byte s){
-  if(s != state){
-  state = s;
-  aniPos = 0;
+void Feeling::setState(byte s) {
+  if (s != state) {
+    state = s;
+    aniPos = 0;
   }
 }
 
-void Feeling::blink(){
+void Feeling::blink() {
   setState(BLINK);
 }
 
 void Feeling::frame() {
   //switch (state) {
-    //case BLINK:
-      if (aniPos == 0) aniPos = random(1,30);
-      if (aniPos < 50) {
-        left.drawBitmap(0, 0, BLINK_0_L, 8, 8, LED_ON);
-        right.drawBitmap(0, 0, BLINK_0_R, 8, 8, LED_ON);
-      } else if (aniPos < 51) {
-        left.drawBitmap(0, 0, BLINK_1_L, 8, 8, LED_ON);
-        right.drawBitmap(0, 0, BLINK_1_R, 8, 8, LED_ON);
-      } else if (aniPos < 52) {
-        left.drawBitmap(0, 0, BLINK_2_L, 8, 8, LED_ON);
-        right.drawBitmap(0, 0, BLINK_2_R, 8, 8, LED_ON);
-      } else {
-        aniPos = 0;
-      }
-      //break;
+  //case BLINK:
+  if (aniPos == 0) aniPos = random(1, 30);
+  if (aniPos < 50) {
+    left.drawBitmap(0, 0, BLINK_0_L, 8, 8, LED_ON);
+    right.drawBitmap(0, 0, BLINK_0_R, 8, 8, LED_ON);
+  } else if (aniPos < 51) {
+    left.drawBitmap(0, 0, BLINK_1_L, 8, 8, LED_ON);
+    right.drawBitmap(0, 0, BLINK_1_R, 8, 8, LED_ON);
+  } else if (aniPos < 52) {
+    left.drawBitmap(0, 0, BLINK_2_L, 8, 8, LED_ON);
+    right.drawBitmap(0, 0, BLINK_2_R, 8, 8, LED_ON);
+  } else {
+    aniPos = 0;
+  }
+  //break;
   //}
 
   left.writeDisplay();
