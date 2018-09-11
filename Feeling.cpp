308f3de9de2646850d6f963f5348bdcc18429072
Anton Pusch <anton.pusch@outlook.de>
2018-09-11 11:08:08 +0200

    started fixing

--- Feeling.cpp
+++ Feeling.cpp
@@ -1,8 +1,11 @@
 #include "Feeling.h"
 
-Feeling::Feeling() {
+Feeling::Feeling() {}
+
+void Feeling::init() {
   left.begin(0x71);  // pass in the address
   right.begin(0x70);
+  Serial.println("set stuff");
 }
 
 void Feeling::setState(byte s) {
@@ -17,23 +20,22 @@ void Feeling::blink() {
 }
 
 void Feeling::frame() {
-  //switch (state) {
-  //case BLINK:
-  if (aniPos == 0) aniPos = random(1, 30);
-  if (aniPos < 50) {
-    left.drawBitmap(0, 0, BLINK_0_L, 8, 8, LED_ON);
-    right.drawBitmap(0, 0, BLINK_0_R, 8, 8, LED_ON);
-  } else if (aniPos < 51) {
-    left.drawBitmap(0, 0, BLINK_1_L, 8, 8, LED_ON);
-    right.drawBitmap(0, 0, BLINK_1_R, 8, 8, LED_ON);
-  } else if (aniPos < 52) {
-    left.drawBitmap(0, 0, BLINK_2_L, 8, 8, LED_ON);
-    right.drawBitmap(0, 0, BLINK_2_R, 8, 8, LED_ON);
-  } else {
-    aniPos = 0;
+  switch (state) {
+    case BLINK:
+      if (aniPos == 0) aniPos = random(1, 30);
+      aniPos %= 52;
+      if (aniPos < 50) {
+        left.drawBitmap(0, 0, BLINK_0_L, 8, 8, LED_ON);
+        right.drawBitmap(0, 0, BLINK_0_R, 8, 8, LED_ON);
+      } else if (aniPos < 51) {
+        left.drawBitmap(0, 0, BLINK_1_L, 8, 8, LED_ON);
+        right.drawBitmap(0, 0, BLINK_1_R, 8, 8, LED_ON);
+      } else {
+        left.drawBitmap(0, 0, BLINK_2_L, 8, 8, LED_ON);
+        right.drawBitmap(0, 0, BLINK_2_R, 8, 8, LED_ON);
+      }
+      break;
   }
-  //break;
-  //}
 
   left.writeDisplay();
   right.writeDisplay();
--- eyes.ino
+++ eyes.ino
@@ -2,14 +2,15 @@
 
 Adafruit_8x8matrix right = Adafruit_8x8matrix();
 Adafruit_8x8matrix left = Adafruit_8x8matrix();
-Feeling feeling;
+Feeling feeling = Feeling();
 
 void setup() {
   Serial.begin(115200);
   Serial.println("8x8 LED Matrix Test");
+  feeling.init();
 }
 
 void loop() {
   feeling.frame();
-  delay(150);
+  delay(10);
 }
